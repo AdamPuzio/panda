3ed4b740907c359905548586040af88ff8b6d4ed
apuzio236 <adam_puzio@cable.comcast.com>
2020-05-17 18:39:54 -0400

    Added app/services manually into runner args

--- bin/runner.js
+++ bin/runner.js
@@ -7,4 +7,5 @@ const { Runner } = require("moleculer");
 const runner = new Runner();
 let args = process.argv
 args.push('node_modules/panda/base/services')
+args.push('app/services')
 runner.start(args);
